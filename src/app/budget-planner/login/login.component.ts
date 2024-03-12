a88feac9c2ddeef58cb451176ce9a66562445b0c
Heet Doshi <doshiheet2872003@gmail.com>
2024-03-12 18:40:34 +0530

    login

--- src/app/budget-planner/login/login.component.ts
+++ src/app/budget-planner/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms'
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm  :any;
+  registerForm : any;
+  activeForm : 'login'| 'refister' ='login'
+
+  constructor(private fb : FormBuilder){}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email : ['',[Validators.required, Validators.email]],
+      password : ['',Validators.required]
+    });
+    this.registerForm = this.fb.group({
+      username : ['',Validators.required],
+      email : ['',[Validators.required, Validators.email]],
+      password : ['',Validators.required]
+    });
+  }
 }
